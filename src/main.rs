af40a9221a560907b268abdcd1c31eb8b63a9f8b
Jakub Okoński <jakub@okonski.org>
2022-09-04 16:55:00 +0200

    Refactor and reduce copies

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use clap::Parser;
 use futures_util::{join, TryFutureExt};
 use std::fmt::Write;
 use std::net::SocketAddr;
+use std::sync::Arc;
 use warp::Filter;
 
 #[derive(Parser, Debug)]
@@ -29,66 +30,86 @@ async fn main() {
         let mut url = reqwest::Url::parse("http://localhost").unwrap();
         url.set_ip_host(capframex_url.ip()).unwrap();
         url.set_port(Some(capframex_url.port())).unwrap();
-        url
+        Arc::new(url)
     };
 
-    let capframex_client = reqwest::Client::builder().build().unwrap();
+    let metric_names = Arc::new(metrics);
+    let capframex_client = Arc::new(reqwest::Client::builder().build().unwrap());
 
-    let handler = warp::path("metrics")
-        .and(warp::any().map(move || capframex_client.clone()))
-        .and(warp::any().map(move || capframex_url.clone()))
-        .and(warp::any().map(move || metrics.clone()))
-        .then(move |capframex_client: reqwest::Client, capframex_url: reqwest::Url, metric_names: Vec<String>| {
-            let x = async move {
-                let processes = async {
-                    capframex_client
-                        .get(capframex_url.join("/api/processes").unwrap())
-                        .send()
-                        .await?
-                        .json()
-                        .await
-                };
-                let metrics = async {
-                    Ok(capframex_client
-                        .get(capframex_url.join(&format!("/api/metrics?metricNames={}", metric_names.join(","))).unwrap())
-                        .send()
-                        .await?
-                        .json::<Vec<f32>>()
-                        .await
-                        .unwrap_or_default())
-                };
-                let (processes, metrics): (
-                    reqwest::Result<Vec<String>>,
-                    reqwest::Result<Vec<f32>>,
-                ) = join!(processes, metrics);
-                let (processes, metrics) = (processes?, metrics?);
-                let mut output = String::new();
-                writeln!(
-                    output,
-                    "# HELP capframex_active_process Process currently being monitored by CapFrameX."
-                )
-                .unwrap();
-                writeln!(output, "# TYPE capframex_active_process gauge").unwrap();
-                for process in processes.iter() {
-                    writeln!(output, "capframex_active_process{{name=\"{}\"}} 1", process).unwrap();
-                }
-                writeln!(
-                    output,
-                    "# HELP capframex_fps Performance metric tracked by CapFrameX."
-                )
-                .unwrap();
-                writeln!(output, "# TYPE capframex_fps gauge").unwrap();
-                for (metric, value) in metric_names.iter().zip(metrics.iter()) {
-                    writeln!(output, "capframex_fps {{name=\"{}\"}} {}", metric, value).unwrap();
-                }
-                Ok(output)
-                // Ok(format!("Hello {processes:?} {metrics:?}"))
-            };
-            x.unwrap_or_else(|x: reqwest::Error| {
-                dbg!(x);
-                "err".to_string()
-            })
-        });
+    let handler = warp::path("metrics").then({
+        let capframex_client = Arc::clone(&capframex_client);
+        let capframex_url = Arc::clone(&capframex_url);
+        let metric_names = Arc::clone(&metric_names);
+
+        move || {
+            let capframex_client = Arc::clone(&capframex_client);
+            let capframex_url = Arc::clone(&capframex_url);
+            let metric_names = Arc::clone(&metric_names);
+            metrics_handler(capframex_client, capframex_url, metric_names)
+        }
+    });
 
     warp::serve(handler).run(bind_address).await;
+}
+
+async fn metrics_handler(
+    capframex_client: Arc<reqwest::Client>,
+    capframex_url: Arc<reqwest::Url>,
+    metric_names: Arc<Vec<String>>,
+) -> String {
+    let x = async move {
+        let processes = async {
+            capframex_client
+                .get(capframex_url.join("/api/processes").unwrap())
+                .send()
+                .await?
+                .json()
+                .await
+        };
+        let metrics = async {
+            Ok(capframex_client
+                .get(
+                    capframex_url
+                        .join(&format!(
+                            "/api/metrics?metricNames={}",
+                            metric_names.join(",")
+                        ))
+                        .unwrap(),
+                )
+                .send()
+                .await?
+                .json::<Vec<f32>>()
+                .await
+                .unwrap_or_default())
+        };
+        let (processes, metrics): (reqwest::Result<Vec<String>>, reqwest::Result<Vec<f32>>) =
+            join!(processes, metrics);
+        let (processes, metrics) = (processes?, metrics?);
+        let mut output = String::new();
+        writeln!(
+            output,
+            "# HELP capframex_active_process Process currently being monitored by CapFrameX."
+        )
+        .unwrap();
+        writeln!(output, "# TYPE capframex_active_process gauge").unwrap();
+        for process in processes.iter() {
+            writeln!(output, "capframex_active_process{{name=\"{}\"}} 1", process).unwrap();
+        }
+        writeln!(
+            output,
+            "# HELP capframex_fps Performance metric tracked by CapFrameX."
+        )
+        .unwrap();
+        writeln!(output, "# TYPE capframex_fps gauge").unwrap();
+        for (metric, value) in metric_names.iter().zip(metrics.iter()) {
+            writeln!(output, "capframex_fps {{name=\"{}\"}} {}", metric, value).unwrap();
+        }
+        Ok(output)
+        // Ok(format!("Hello {processes:?} {metrics:?}"))
+    };
+    x.unwrap_or_else(|x: reqwest::Error| {
+        dbg!(x);
+        "err".to_string()
+    })
+    .await
 }
